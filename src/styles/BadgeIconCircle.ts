91565381345764f846adcf45df432dc630bbc191
Sanarielsen <sanarielsen.dev@gmail.com>
2024-07-23 16:44:33 -0300

    feat(SNC-03): create and add badge description component in a home
    Create badge to show description outside of badge icon as used in Cart
    icon, I was created a new component to show a icon with description
    beside of it and added in the home screen.

---
+++ src/styles/BadgeIconCircle.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BadgeContainerCircle = styled.div`
+
+  display: flex;
+  border-radius: 50%;
+  margin: 8px;
+  padding: 8px;
+  cursor: pointer;
+`;
