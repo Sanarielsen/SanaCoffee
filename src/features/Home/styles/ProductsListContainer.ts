ce84473971686f825dc7552d1197c1ddd1a7e1b5
Sanarielsen <sanarielsen.dev@gmail.com>
2024-07-25 12:49:14 -0300

    feat(SNC-04): create mockup of list products
    Create initial mockup to build the list of products in the home page
    
    Resolves: SNC-04-01

---
+++ src/features/Home/styles/ProductsListContainer.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ProductsListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  padding: 16px;
+`;
