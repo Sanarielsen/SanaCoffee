be3be97cde1a8464221ed5e1936a075ace52f1d9
Sanarielsen <sanarielsen.dev@gmail.com>
2024-07-25 12:49:14 -0300

    feat(SNC-04): create list of itens in catalog for home page
    Create item of catalog and add mock to show how can I show itens for the
    user in the home page.
    
    In this commit, we need to create ButtonIcon, BadgeTopic and InputRange
    to create all structure necessary for the feature.
    
    Change color of focus and hover components in footer catalog because the
    color used was not in the color palette.
    
    Organize styles and components inside of Home feature because previously
    organization caused a lot of files with 5 lines of code. Now, the
    organization is more clear and easy to understand because all components
    is located in same file with "Container" in the final.

---
+++ src/features/Home/components/ProductItemFooter.tsx
@@ -0,0 +1,44 @@
+import { ButtonIcon } from "@globalComponents/ButtonIcon";
+import {
+  PanelContainer,
+  MoneyMetric,
+  MoneyValue,
+} from "@features/Home/styles/ProductItemFooterContainer";
+import { InputRange } from "@globalComponents/InputRange";
+import { useState } from "react";
+
+interface ProductItemFooterProps {
+  nameRange: string;
+  value: number;
+}
+
+export function ProductItemFooter({ nameRange, value }: ProductItemFooterProps) {
+  const [quantityProduct, setQuantityProduct] = useState(0);
+
+  const handleClickAddCurrentItemOnCart = () => {
+    //Add logic to add item on cart
+    setQuantityProduct(() => 0);
+  }
+
+  //Component capable receive value of product with ever 3 numbers in total.
+  return (
+    <PanelContainer>
+      <div>
+        <MoneyMetric>R$</MoneyMetric>
+        <MoneyValue>{value}</MoneyValue>
+      </div>
+      <div>
+        <InputRange
+          name={nameRange}
+          value={quantityProduct}
+          onChange={(currentQuantity) => {
+            setQuantityProduct(currentQuantity);
+          }}
+        />
+      </div>
+      <div>
+        <ButtonIcon pathImage="/IconCart.svg" onClick={handleClickAddCurrentItemOnCart} />
+      </div>
+    </PanelContainer>
+  );
+}
