71822a08953136bae99864704b1d21af067d2973
Sanarielsen <sanarielsen.dev@gmail.com>
2024-08-06 15:44:18 -0300

    fix: remove fixed height and adapt positioning of topics on banner

--- src/styles/BadgeCircleBody.ts
+++ src/styles/BadgeCircleBody.ts
@@ -1,8 +1,17 @@
 import styled from "styled-components";
+import { device } from "./Devices";
 
 export const BadgeCircleBody = styled.div`
   display: flex;
   align-items: center;
 
   color: #574F4D;
+
+  @media ${device.laptop} { 
+    justify-content: center;
+  }
+
+  @media ${device.laptopL} { 
+    justify-content: left;
+  }
 `;
