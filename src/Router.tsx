a4cd564a10de6f2235d93a5fe455c87a302d7edf
Sanarielsen <sanarielsen.dev@gmail.com>
2024-11-05 22:20:42 -0300

    feat(SNC-08): create all header for this page
    Create all configuration about routes and default layout to header page
    and all things I need to continue this part of application.
    
    Resolves: SNC-08-01

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './features/Home/page/HomePage';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { CartPage } from '@features/Cart/page/CartPage';
+import { FinishedOrderPage } from '@features/Review/page/FinishedOrderPage';
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/carrinho" element={<CartPage />} />
+        <Route path="/finalizado" element={<FinishedOrderPage />} />
       </Route>
     </Routes>
   );
---
+++ src/features/Review/page/FinishedOrderPage.tsx
@@ -0,0 +1,11 @@
+import { DescriptionHeader, MainContainer, TitleHeader } from "@features/Review/styles/FinishedOrderPageContainer";
+
+export function FinishedOrderPage() {
+
+  return (
+    <MainContainer>
+      <TitleHeader>Uhu! Pedido confirmado</TitleHeader>
+      <DescriptionHeader>Agora é só aguardar que logo o café chegará até você</DescriptionHeader>
+    </MainContainer>
+  );
+}
