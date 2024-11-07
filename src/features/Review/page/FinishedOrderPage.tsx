1665a4a5fb45d3cc4c780f2c7fdaa1d94b1f0eb7
Sanarielsen <sanarielsen.dev@gmail.com>
2024-11-06 22:17:29 -0300

    feat(SNC-08): create two sections relationed of Order
    Create two sections, basically we have informations about the order
    succeed and image to help a simbolize of delivery is actually comming
    to you.
    
    Resolves: SNC-08-02

--- src/features/Review/page/FinishedOrderPage.tsx
+++ src/features/Review/page/FinishedOrderPage.tsx
@@ -1,4 +1,5 @@
 import { DescriptionHeader, MainContainer, TitleHeader } from "@features/Review/styles/FinishedOrderPageContainer";
+import { OrderInformations } from "../components/OrderInformations";
 
 export function FinishedOrderPage() {
 
@@ -6,6 +7,8 @@ export function FinishedOrderPage() {
     <MainContainer>
       <TitleHeader>Uhu! Pedido confirmado</TitleHeader>
       <DescriptionHeader>Agora é só aguardar que logo o café chegará até você</DescriptionHeader>
+
+      <OrderInformations />
     </MainContainer>
   );
 }
---
+++ src/features/Review/styles/OrderInformationsContainer.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 2;
+  gap: 102px;
+  padding-top: 40px;
+`;
+
+export const OrderSection = styled.div`
+  grid-column: 1;
+  padding: 8px 0 40px 40px;
+  background-color: #000;
+  color: #FFF;
+  border-radius: 6px 36px;
+`;
+
+export const TopicOrder = styled.div`
+  padding-top: 32px;
+`;
+
+export const DeliveryImageSection = styled.div`
+  grid-column: 2;
+`;
+
