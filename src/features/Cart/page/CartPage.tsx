06187ce30d19f7477ec8e96be1537e9fadbb3fce
Sanarielsen <sanarielsen.dev@gmail.com>
2025-04-23 14:40:11 -0300

    feat(SNC-12): create an action to finish the process of buying
    Create an action to finish the process of buying when everything is done
    and need to be reviewed by the user.
    
    The modal for confimation is already used here for confirm an action.

--- src/features/Cart/page/CartPage.tsx
+++ src/features/Cart/page/CartPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -9,30 +10,31 @@ import {
   CartPageProducts,
 } from '@features/Cart/styles/CartPageContainer';
 import { CartPayment } from '@features/Cart/components/CartPayment';
-import { CartPersonAddress } from '@features/Cart/types/CartAddress';
+import { CartPersonAddress } from '@globalTypes/CartAddress';
 import { CartReview } from '@features/Cart/components/CartReview';
 import { cartSchema } from '@features/Cart/schemas/cartSchema';
-import { useCartProducts } from 'src/contexts/CartProductsContext';
+import { ModalConfirmation } from '@globalComponents/ModalConfirmation';
 
 export function CartPage() {
   const navigate = useNavigate();
   const contextForms = useForm({
     resolver: yupResolver(cartSchema),
   });
-  
-  const { getCartItems } = useCartProducts();
 
-  const onSubmitDelivery: SubmitHandler<CartPersonAddress> = (data) => {
-    console.log('Pedido realizado com sucesso: ', data);
+  const [ modalState, setModalState ] = useState(false);  
+  //const { getCartItems } = useCartProducts();
+
+  const onSubmitDelivery: SubmitHandler<CartPersonAddress> = () => {    
+    setModalState(true);
+  };
+
+  const handleSendSubmitOrder = () => {
 
     const addressValues = contextForms.getValues();
 
-    //We have a query to send the data to the backend
-    console.log('Endereco: ', addressValues);
-    console.log('Cart: ', getCartItems);
-
-    navigate('/finalizado');
-  };
+    // FEAT: We need a algoritm to calculate the delivery time of current order
+    navigate('/finalizado', { state: addressValues });
+  }
 
   return (
     <FormProvider {...contextForms}>
@@ -50,6 +52,14 @@ export function CartPage() {
           <CartReview />
         </CartPageProducts>
       </CartPagePanel>
+
+      <ModalConfirmation 
+        open={modalState}
+        title="Confirmar o pedido atual"
+        message="Deseja realmente confirmar o pedido atual?"
+        onClickConfirm={() => handleSendSubmitOrder()} 
+        onClickClose={() => setModalState(false)}
+      />
     </FormProvider>
   );
 }
--- src/features/Review/page/FinishedOrderPage.tsx
+++ src/features/Review/page/FinishedOrderPage.tsx
@@ -1,12 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
 import { MainContainer } from '@features/Review/styles/FinishedOrderPageContainer';
 import { OrderHeader } from '@features/Review/components/OrderHeader';
 import { OrderInformations } from '@features/Review/components/OrderInformations';
+import { CartPersonAddress } from '@globalTypes/CartAddress';
 
 export function FinishedOrderPage() {
+
+  const locationVariables = useLocation();
+  const userAddress = locationVariables.state as CartPersonAddress;  
+
   return (
     <MainContainer>
       <OrderHeader />
-      <OrderInformations />
+      <OrderInformations address={userAddress} />
     </MainContainer>
   );
 }
